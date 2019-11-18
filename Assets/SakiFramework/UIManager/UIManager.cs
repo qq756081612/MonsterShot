86f835cfdeaa9f5232713a5782e98d18bf4f2cb6
SakiQAQ <756081612@qq.com>
2019-11-18 16:01:19 +0800

    #UIManager

---
+++ Assets/SakiFramework/UIManager/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUIManager
+{
+    //主要功能
+    //1.负责UI资源的加载/缓存/卸载
+    //2.管理UI及UI特效层级
+
+    //接口
+    //1.显示UI
+    //2.隐藏UI
+}
+
+
+public class UIManager : IUIManager
+{
+
+}
